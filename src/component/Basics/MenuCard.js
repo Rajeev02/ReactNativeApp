9e28758b1d84c2b54f4e9256030c45d9c1c6a5b8
Rajeev Joshi <rajeev.joshi@letsventure.com>
2022-04-09 14:38:49 +0530

    restaurant basic app complete

---
+++ src/component/Basics/MenuCard.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+function MenuCard({ menuData }) {
+  console.log(menuData);
+
+  return (
+    <>
+      <section className="main-card--cointainer">
+        {menuData.map((curElement) => {
+          const { id, name, category, image, description } = curElement;
+          return (
+            <>
+              <div className="card-container" key={id}>
+                <div className="card">
+                  <div className="card-body">
+                    <span className="card-number card-circle subtl">{id}</span>
+                    <span className="card-author subtle">{category}</span>
+                    <h2 className="card-title">{name}</h2>
+                    <span className="card-description subtl">
+                      {description}
+                    </span>
+                    <div className="card-read">Read</div>
+                  </div>
+                  <img src={image} alt="images" className="card-media" />
+                  <span className="card-tag subtle">Order Now</span>
+                </div>
+              </div>
+            </>
+          );
+        })}
+      </section>
+    </>
+  );
+}
+
+export default MenuCard;
